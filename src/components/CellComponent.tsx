92e55fdb0d1e0ace73df78cf78d9fc9d3a822ca4
Mykola Batrak <kolyaun@live.com>
2024-09-02 23:55:14 +0300

    Added figures component

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -2,15 +2,15 @@ import React, { FC } from 'react';
 import { Cell } from '../models/Cell';
 
 interface CellProps {
-    cell: Cell
-}
+    cell: Cell    
+} 
 
 const CellComponent: FC<CellProps> = ({cell}) => {
     return (
         <div
         className={['cell', cell.color].join(' ')}
         >
-
+            {cell.figure}
         </div>
     );
 }
