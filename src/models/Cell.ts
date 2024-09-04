c5d3b167a91d28db13282318d493814da5d748d9
Mykola Batrak <kolyaun@live.com>
2024-09-04 23:30:35 +0300

    added method for cell

--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -20,4 +20,12 @@ export class Cell {
         this.available = false;
         this.id = new Date().valueOf();
     }
+
+    moveFigure(target: Cell) {
+        if (this.figure && this.figure?.canMove(target)) {
+            this.figure.moveFigure(target)
+            target.figure = this.figure;
+            this.figure = null;
+        }
+    }
 }
