cc2b2fd2fd02a01cc5ac45f0aa936058d84c70b2
Mykola Batrak <kolyaun@live.com>
2024-09-09 18:33:02 +0300

    added Lost list figures component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import BoardComponent from './components/BoardComponent';
-import { Board } from './models/Board';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import BoardComponent from "./components/BoardComponent";
+import { Board } from "./models/Board";
+import { Player } from "./models/Player";
+import { Colors } from "./models/Colors";
+import LostFigures from "./components/LostFigures";
 
 const App = () => {
   const [board, setBoard] = useState(new Board());
-  
+  const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE));
+  const [blackPlayer, setblackPlayer] = useState(new Player(Colors.BLACK));
+  const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null);
+
   useEffect(() => {
-    restart()
-  }, [])
+    restart();
+    setCurrentPlayer(whitePlayer);
+  }, []);
 
   function restart() {
     const newBoard = new Board();
@@ -17,14 +24,26 @@ const App = () => {
     setBoard(newBoard);
   }
 
+  function swapPlayer() {
+    setCurrentPlayer(
+      currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer
+    );
+  }
+
   return (
-    <div className='app'>
+    <div className="app">
       <BoardComponent
         board={board}
         setBoard={setBoard}
+        currentPlayer={currentPlayer}
+        swapPlayer={swapPlayer}
       />
+      <div>
+        <LostFigures title="Black figures" figures={board.lostBlackFigures} />
+        <LostFigures title="White figures" figures={board.lostWhiteFigures} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
